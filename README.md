2c1333de8a18eaa9a9c2cbabbdc81a4479b61a07
Loc Nguyen <me@locnh.com>
2019-03-27 19:15:02 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a python script to collect health-check of a docker repository and send to DataDog API for monitoring.
---
+++ monitor.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Import the SDKs
+import datadog
+import docker
+import time
+
+# Credentials
+docker_hub_username = ""
+docker_hub_password = ""
+
+datadog_api_key = ""
+datadog_app_key = ""
+
+# Main
+is_healthy = True
+
+# Check
+try:
+    client = docker.from_env()
+    client.login(username=docker_hub_username, password=docker_hub_password)
+
+    # Clean up
+    try:
+        print("Cleaning the floor ..")
+        for container in client.containers.list(all):
+            container.remove(force=True)
+
+        for image in client.images.list():
+            client.images.remove(image.id)
+    except:
+        pass
+
+    print("Pulling image ...")
+    client.images.pull("busybox", tag="latest")
+
+    print("Starting container ...")
+    container = client.containers.run("busybox", "date > /check.log", detach=True)
+
+    print("Making change and commit to image ...")
+    container.commit("locnh/busybox", tag="test")
+
+    print("Pushing image ...")
+    running_time = time.time()
+    client.images.push("locnh/busybox:test")
+    running_time = time.time() - running_time
+except:
+    is_healthy = False
+
+if is_healthy:
+    print("I'm good")
+    print("Pushing in %s seconds" % running_time)
+else:
+    print("Having cancer")
+
+# Datadog sending
+options = {
+    'api_key': datadog_api_key,
+    'app_key': datadog_app_key
+}
+
+print("Sending metrics to datadog ...")
+datadog.initialize(**options)
+datadog.api.Metric.send(metric='registry.healthcheck.runningtime', points=(time.time(), running_time))
+datadog.api.Metric.send(metric='registry.healthcheck.is_healthy', points=(time.time(), is_healthy))
